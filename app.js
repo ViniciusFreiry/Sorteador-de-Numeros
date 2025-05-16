4591a887804a9a3732bbcb94c1d7ce38c757383b
ViniciusFreiry <viniciusfreiry@gmail.com>
2025-05-16 15:43:12 -0300

    Getting HTML Elements

---
+++ app.js
@@ -0,0 +1,13 @@
+const inputQuantidade = document.getElementById("quantidade");
+const inputDe = document.getElementById("de");
+const inputAte = document.getElementById("ate");
+const botaoSortear = document.getElementById("btn-sortear");
+const botaoReiniciar = document.getElementById("btn-reiniciar");
+
+function sortear() {
+    
+}
+
+function reiniciar() {
+
+}
